9eae866bf2c68c5eca20e7f08a8398b4f0eb31df
jon-a-nygaard <jon.arild@highsoft.com>
2013-09-11 12:10:43 +0200

    Added manifest and plugin file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 export-csv
 ==========
+This feature allows the user to export the chart data to a csv file.
+
+The contents of the plugin is located in the javascript file "export-csv.js". 
+This plugin is published under the MIT license, and the license document is included in the repository.
---
+++ export-csv.js
@@ -0,0 +1,55 @@
+/**
+ * A small plugin for getting the CSV of a categorized chart
+ */
+(function (Highcharts) {
+    
+    // Options
+    var itemDelimiter = ',',  // use ';' for direct import to Excel
+        lineDelimiter = '\n';
+        
+    var each = Highcharts.each;
+    Highcharts.Chart.prototype.getCSV = function () {
+        var xAxis = this.xAxis[0],
+            columns = [],
+            line,
+            csv = "", 
+            row,
+            col;
+        
+        if (xAxis.categories) {
+            columns.push(xAxis.categories);
+            columns[0].unshift("");
+        }
+        each (this.series, function (series) {
+            columns.push(series.yData);
+            columns[columns.length - 1].unshift(series.name);
+        });
+        
+        // Transform the columns to CSV
+        for (row = 0; row < columns[0].length; row++) {
+            line = [];
+            for (col = 0; col < columns.length; col++) {
+                line.push(columns[col][row]);
+            }
+            csv += line.join(itemDelimiter) + lineDelimiter;
+        }
+        return csv;
+    };   
+
+    // Now we want to add "Download CSV" to the exporting menu. We post the CSV
+    // to a simple PHP script that returns it with a content-type header as a 
+    // downloadable file.
+    // The source code for the PHP script can be viewed at 
+    // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
+
+    Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
+        text: 'Download CSV',
+        onclick: function () {
+            Highcharts.post('http://www.highcharts.com/studies/csv-export/csv.php', {
+                csv: this.getCSV()
+            });
+        }
+    }); 
+}(Highcharts));
+
+
