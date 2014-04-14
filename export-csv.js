c0302b56395b58440c311e8b18d570393e710be4
Rokas Šleinius <raveren@gmail.com>
2014-04-14 15:27:39 +0300

    rewrote csv generation plus some extra features
    * y-values for same x-values from different series are placed in same row
    * x-values no longer duplicated as columns for each series
    * X column title and downloaded file title is taken from user provided values when configuring the chart if available.
    * Browser-side download is used if supported by browser bypassing server-side script
    * Default title of chart is passed to backend along with csv string.
    
    known issues: multiple x axes (no longer?) supported

--- export-csv.js
+++ export-csv.js
@@ -1,17 +1,19 @@
 /**
- * A small plugin for getting the CSV of a categorized chart
+ * A small plugin for getting the CSV of a categorized chart v 1.0.3
+ * https://github.com/highslide-software/export-csv
+ * http://www.highcharts.com/plugin-registry/single/7/Export-CSV
  */
- (function (Highcharts) {
-    
-    
-    var each = Highcharts.each;
-    Highcharts.Chart.prototype.downloadCSV = function ( returnOutput ) {
-        var columns = [],
+(function( Highcharts ) {
+
+    Highcharts.Chart.prototype.downloadCSV = function( returnOutput ) {
+        var titles = [],
+            xValues = [],
             line,
             csv = "",
-            row,
             col,
             options = (this.options.exporting || {}).csv || {},
+            totalSeries = 0,
+            xNeedsSorting = false,
 
             // Options
             dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S',
@@ -23,59 +25,102 @@
             url = options.url || 'http://www.highcharts.com/studies/csv-export/csv.php',
             lineDelimiter = options.lineDelimeter || '\n';
 
-        each (this.series, function (series) {
-            if (series.options.includeInCSVExport !== false) {
-                if (series.xAxis) {
-                    var xData = series.xData.slice(),
-                        xTitle = 'X values';
-                    if (series.xAxis.isDatetimeAxis) {
-                        xData = Highcharts.map(xData, function (x) {
-                            return Highcharts.dateFormat(dateFormat, x)
-                        });
-                        xTitle = 'DateTime';
-                    } else if (series.xAxis.categories) {
-                        xData = Highcharts.map(xData, function (x) {
-                            return Highcharts.pick(series.xAxis.categories[x], x);
-                        });
-                        xTitle = 'Category';
-                    }
-                    columns.push(xData);
-                    columns[columns.length - 1].unshift(xTitle);
-                }
-                columns.push(series.yData.slice());
-                columns[columns.length - 1].unshift(series.name);
+        Highcharts.each(this.series, function( series, seriesNo ) {
+            if ( series.options.includeInCSVExport === true ) return;
+            if ( series.options.xAxis ) { // if option is set and not zero
+                alert("Downloading charts as CSV with multiple axes is no supported, sorry.");
+                throw false;
             }
+            if ( titles.length === 0 ) {
+                var xTitle = ((series.xAxis.options || {}).title || {}).text;
+
+                if ( xTitle ) {
+                    // series.xAxis.title.text exists and we use it
+                } else if ( series.xAxis.isDatetimeAxis ) {
+                    xTitle = 'DateTime';
+                    xNeedsSorting = true;
+                } else if ( series.xAxis.categories ) {
+                    xTitle = 'Category';
+                } else {
+                    xTitle = 'X values';
+                }
+                titles.push(xTitle);
+            }
+
+            titles.push(series.name);
+            totalSeries++;
         });
 
-        // Transform the columns to CSV
-        for (row = 0; row < columns[0].length; row++) {
+        Highcharts.each(this.series, function( series, seriesNo ) {
+            if ( series.options.includeInCSVExport === true ) return;
+
+            Highcharts.each(series.data, function( point ) {
+                var l = xValues.length;
+                while ( l-- ) {
+                    if ( xValues[l].x === point.x ) {
+                        xValues[l].y[seriesNo] = point.y;
+                        return;
+                    }
+                }
+
+                var y = new Array(totalSeries);
+                y[-1] = point.x; // -1nth element holds formatted x axis value
+                y[seriesNo] = point.y;
+
+                if ( series.xAxis.isDatetimeAxis ) {
+                    y[-1] = Highcharts.dateFormat(dateFormat, point.x);
+                } else if ( series.xAxis.categories ) {
+                    y[-1] = Highcharts.pick(series.xAxis.categories[point.x], point.x);
+                }
+
+                xValues.push({ x : point.x, y : y });
+            })
+        });
+
+        if ( xNeedsSorting ) {
+            xValues.sort(function( a, b ) {return a.x - b.x});
+        }
+        csv = titles.join(itemDelimiter) + lineDelimiter;
+        Highcharts.each(xValues, function( values ) {
             line = [];
-            for (col = 0; col < columns.length; col++) {
-                line.push(columns[col][row]);
+            for ( col = -1; col < values.y.length; col++ ) {
+                line.push(values.y[col]);
             }
             csv += line.join(itemDelimiter) + lineDelimiter;
-        }
-        
-	if ( returnOutput ) {
+        });
+
+        if ( returnOutput ) {
             return csv;
-	} else {
-            Highcharts.post(url, { csv : csv });
-	}
+        } else {
+            var title = ((this.title || {}).text || 'chart') + '.csv',
+                a = document.createElement('a');
+
+            if ( "download" in a ) { // modern browser - no need to use backend script
+                a.href = 'data:attachment/csv,' + encodeURIComponent(csv);
+                a.target = '_blank';
+                a.download = title;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            } else {
+                Highcharts.post(url, { csv : csv, title : title });
+            }
+        }
     };
-    
-    Highcharts.Chart.prototype.getCSV = function () {
-	return this.downloadCSV(true);
-    }
+
+    Highcharts.Chart.prototype.getCSV = function() {
+        return this.downloadCSV(true);
+    };
 
     // Now we want to add "Download CSV" to the exporting menu. We post the CSV
-    // to a simple PHP script that returns it with a content-type header as a 
+    // to a simple PHP script that returns it with a content-type header as a
     // downloadable file.
-    // The source code for the PHP script can be viewed at 
+    // The source code for the PHP script can be viewed at
     // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
-    if (Highcharts.getOptions().exporting) {
+    if ( Highcharts.getOptions().exporting ) {
         Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
-            text: Highcharts.getOptions().lang.downloadCSV || "Download CSV",
-            onclick : function () { this.downloadCSV() }
+            text    : Highcharts.getOptions().lang.downloadCSV || "Download CSV",
+            onclick : function() { this.downloadCSV() }
         });
     }
 }(Highcharts));
