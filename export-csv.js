b40cdda1e66e9d19775723de0db7046f7b0b9584
tomascassidy <tomas.cassidy@gmail.com>
2014-08-18 11:13:24 +1000

    Update code comment for CSV downloading. Change data URI mime type to "text/csv". Add UTF-8 as specific charset to data URI. Fix for returning CSV and setting default URL.
    Fixes #20

--- export-csv.js
+++ export-csv.js
@@ -58,6 +58,14 @@
     };
 
     // Now we want to add "Download CSV" to the exporting menu.
+    // If supported by the browser we use the download attribute
+    // on an anchor element to tell the browser to return the url
+    // data as a downloadable file.
+    // If the download attribute is not supported we post the CSV
+    // to a simple PHP script that returns it with a content-type
+    // header as a downloadable file.
+    // The source code for the PHP script can be viewed at
+    // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
     if (Highcharts.getOptions().exporting) {
         Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
             text: Highcharts.getOptions().lang.downloadCSV || "Download CSV",
@@ -67,15 +75,15 @@
                     a = document.createElement('a');
 
                 if ("download" in a) { // modern browser - no need to use backend script
-                    a.href = 'data:attachment/csv,' + encodeURIComponent(csv);
+                    a.href = 'data:text/csv;charset=UTF-8,' + encodeURIComponent(csv);
                     a.target = '_blank';
                     a.download = title;
                     document.body.appendChild(a);
                     a.click();
                     a.remove();
                 } else {
-                    Highcharts.post(url, {
-                        csv: csv,
+                    Highcharts.post('http://www.highcharts.com/studies/csv-export/csv.php', {
+                        csv: this.getCSV(),
                         title: title
                     });
                 }
