e1cf8871bcfe6a00ef422148f36b1f8d1a14e2a2
danreb25 <gfa.stylog@mls.nc>
2014-09-12 13:27:30 +1100

    check for download attribute support
    check for download attribute support
     use server side extraction if not supported
     use client side extraction if supported

--- export-csv.js
+++ export-csv.js
@@ -5,6 +5,10 @@
     
     
     var each = Highcharts.each;
+	// check if we can download on client side
+	var downloadAttrSupported = !!("download" in document.createElement("a"));
+
+
     Highcharts.Chart.prototype.getCSV = function () {
         var columns = [],
             line,
@@ -17,8 +21,9 @@
 
             // Options
             dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S',
-            itemDelimiter = options.itemDelimiter || ',', // use ';' for direct import to Excel
-            lineDelimiter = options.lineDelimiter || "%0A"; // '\n' isn't working with the js csv data extraction
+            itemDelimiter = options.itemDelimiter || ';', // use ';' for direct import to Excel
+			lineDelimiter = (downloadAttrSupported ? options.lineDelimiter || "%0A" :options.lineDelimiter || '\n\r'); // '\n' isn't working with the js csv data extraction
+
 
         each (this.series, function (series) {
             if (series.options.includeInCSVExport !== false) {
@@ -57,31 +62,53 @@
         return csv;
     };
 
-    // Now we want to add "Download CSV" to the exporting menu. We post the CSV
-    // to a simple PHP script that returns it with a content-type header as a 
+    // Now we want to add "Download CSV" to the exporting menu. 
+	// If the browser do support the downlod attrinute
+	//		then extraction is done on Client side
+	//		else extraction is done on Server side
+	// For the Client side extraction we addapt the code from 
+	//		 http://stackoverflow.com/questions/17836273/export-javascript-data-to-csv-file-without-server-interaction
+	// For the Server Side Extraction 
+	// We post the CSV
+    // to a simple PHP script that returns it with a content-type header as a
     // downloadable file.
-    // The source code for the PHP script can be viewed at 
+    // The source code for the PHP script can be viewed at
     // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
-    // Instead of the php script on server side we get the csv info on client side
-    // original code  from 
-    // http://stackoverflow.com/questions/17836273/export-javascript-data-to-csv-file-without-server-interaction
-    if (Highcharts.getOptions().exporting) {
-        Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
-            text: Highcharts.getOptions().lang.downloadCSV || "Download CSV",
-            onclick: function () {
-     			var a         = document.createElement('a');
-    			a.href        = 'data:attachment/csv,' + this.getCSV();
-    			a.target      = '_blank';
-    			var fileName='savedChart';
-    			if (this.title){
-    				fileName= this.title.textStr.replace(/ /g ,'_') 
-    			}
-    			a.download    = fileName + '.csv';
-    			document.body.appendChild(a);
-    			a.click();
-    			a.remove();
-            }
-        });
-    }
-}(Highcharts));
+	if (Highcharts.getOptions().exporting) {
+		Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
+			text: Highcharts.getOptions().lang.downloadCSV || "Download CSV",
+			onclick: function () {
+			var a = document.createElement('a');
+			
+			if(typeof a.download != "undefined")
+			{
+				// download attribute is supported
+				// Client side extraction
+				a.href        = 'data:attachment/csv,' + this.getCSV();
+				a.target      = '_blank';
+				var fileName='savedChart';
+				if (this.title){
+					fileName= this.title.textStr.replace(/ /g ,'_') 
+				}
+				a.download    = fileName + '.csv';
+				document.body.appendChild(a);
+				a.click();
+			}
+			else
+			{
+			  // download attribute is not supported
+			  // Server side extraction
+				Highcharts.post('http://www.highcharts.com/studies/csv-export/csv.php', {
+					csv: this.getCSV()
+				});
+			}
 
+			a.remove();
+			}
+		});
+	}
+	
+	
+	
+	
+}(Highcharts));
