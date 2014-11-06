cff8c1c60361c17a213a05d91136716c7056c2b2
Torstein Hønsi <torstein@highsoft.com>
2014-11-06 09:39:09 +0100

    Split getCSV into two methods, getDataRows and getCSV
    This allows a hook for getting the raw data rows for use in for
    instance a HTML table.

--- export-csv.js
+++ export-csv.js
@@ -10,20 +10,22 @@
         downloadAttrSupported = document.createElement('a').download !== undefined;
 
 
-    Highcharts.Chart.prototype.getCSV = function () {
+    /**
+     * Get the data rows as a two dimensional array
+     */
+    Highcharts.Chart.prototype.getDataRows = function () {
         var csv,
             options = (this.options.exporting || {}).csv || {},
             xAxis = this.xAxis[0],
             rows = {},
             rowArr = [],
+            dataRows,
             names = [],
             i,
             x,
 
             // Options
-            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S',
-            itemDelimiter = options.itemDelimiter || ';', // use ';' for direct import to Excel
-            lineDelimiter = options.lineDelimiter || '\n'; // '\n' isn't working with the js csv data extraction
+            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S';
 
         // Loop the series and index values
         i = 0;
@@ -59,27 +61,68 @@
         });
 
         // Add header row
-        csv = (xAxis.isDatetimeAxis ? 'DateTime' : 'Category') + itemDelimiter +
-            names.join(itemDelimiter) + lineDelimiter;
+        dataRows = [[xAxis.isDatetimeAxis ? 'DateTime' : 'Category'].concat(names)];
 
         // Transform the rows to CSV
         each(rowArr, function (row, i) {
 
             // Add the X/date/category
-            csv += row.name || (xAxis.isDatetimeAxis ? Highcharts.dateFormat(dateFormat, row.x) : xAxis.categories ? Highcharts.pick(xAxis.categories[row.x], row.x) : row.x);
-            csv += itemDelimiter;
+            row.unshift(row.name || (xAxis.isDatetimeAxis ? Highcharts.dateFormat(dateFormat, row.x) : xAxis.categories ? Highcharts.pick(xAxis.categories[row.x], row.x) : row.x));
+            dataRows.push(row);
+        });
+
+        return dataRows;
+    };
+
+    /**
+     * Get a CSV string
+     */
+    Highcharts.Chart.prototype.getCSV = function () {
+        var csv = '',
+            rows = this.getDataRows(),
+            options = (this.options.exporting || {}).csv || {},
+            itemDelimiter = options.itemDelimiter || ';', // use ';' for direct import to Excel
+            lineDelimiter = options.lineDelimiter || '\n'; // '\n' isn't working with the js csv data extraction
+
+        // Transform the rows to CSV
+        each(rows, function (row, i) {
 
             // Add the values
             csv += row.join(itemDelimiter);
 
             // Add the line delimiter
-            if (i < rowArr.length - 1) {
+            if (i < rows.length - 1) {
                 csv += lineDelimiter;
             }
         });
-
         return csv;
     };
+
+    /**
+     * Build a HTML table with the data
+     * /
+    Highcharts.Chart.prototype.getTable = function () {
+        var html = '<table>',
+            rows = this.getDataRows(),
+            options = (this.options.exporting || {}).csv || {};
+
+        // Transform the rows to HTML
+        each(rows, function (row, i) {
+
+            var tag = i ? 'td' : 'th';
+
+            html += '<tr><' + tag + '>';
+
+            // Add the cells
+            html += row.join('</' + tag + '><' + tag + '>');
+
+            html += '</' + tag + '></tr>';
+            
+        });
+        html += '</table>';
+        return html;
+    };
+    */
 
     // Add "Download CSV" to the exporting menu. Use download attribute if supported, else
     // run a simple PHP script that returns a file. The source code for the PHP script can be viewed at
