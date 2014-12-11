fa0f71c8d007c754fa737c4a39966a7863df28ff
SirAlexiner <Sir_Alexiner@hotmail.com>
2014-12-11 15:42:03 +0100

    v1.2.0

--- export-csv.js
+++ export-csv.js
@@ -1,7 +1,7 @@
 /**
  * A small plugin for getting the CSV of a rendered chart
  */
-/*global Highcharts, document */
+/*global Highcharts, window, document, Blob */
 (function (Highcharts) {
 
     'use strict';
@@ -10,20 +10,21 @@
         downloadAttrSupported = document.createElement('a').download !== undefined;
 
 
-    Highcharts.Chart.prototype.getCSV = function () {
-        var csv,
-            options = (this.options.exporting || {}).csv || {},
+    /**
+     * Get the data rows as a two dimensional array
+     */
+    Highcharts.Chart.prototype.getDataRows = function () {
+        var options = (this.options.exporting || {}).csv || {},
             xAxis = this.xAxis[0],
             rows = {},
             rowArr = [],
+            dataRows,
             names = [],
             i,
             x,
 
             // Options
-            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S',
-            itemDelimiter = options.itemDelimiter || ';', // use ';' for direct import to Excel
-            lineDelimiter = options.lineDelimiter || '\n'; // '\n' isn't working with the js csv data extraction
+            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S';
 
         // Loop the series and index values
         i = 0;
@@ -59,27 +60,93 @@
         });
 
         // Add header row
-        csv = (xAxis.isDatetimeAxis ? 'DateTime' : 'Category') + itemDelimiter +
-            names.join(itemDelimiter) + lineDelimiter;
+        dataRows = [[xAxis.isDatetimeAxis ? 'DateTime' : 'Category'].concat(names)];
 
         // Transform the rows to CSV
-        each(rowArr, function (row, i) {
+        each(rowArr, function (row) {
 
             // Add the X/date/category
-            csv += row.name || (xAxis.isDatetimeAxis ? Highcharts.dateFormat(dateFormat, row.x) : xAxis.categories ? Highcharts.pick(xAxis.categories[row.x], row.x) : row.x);
-            csv += itemDelimiter;
+            row.unshift(row.name || (xAxis.isDatetimeAxis ? Highcharts.dateFormat(dateFormat, row.x) : xAxis.categories ? Highcharts.pick(xAxis.categories[row.x], row.x) : row.x));
+            dataRows.push(row);
+        });
+
+        return dataRows;
+    };
+
+    /**
+     * Get a CSV string
+     */
+    Highcharts.Chart.prototype.getCSV = function () {
+        var csv = '',
+            rows = this.getDataRows(),
+            options = (this.options.exporting || {}).csv || {},
+            itemDelimiter = options.itemDelimiter || ';', // use ';' for direct import to Excel
+            lineDelimiter = options.lineDelimiter || '\n'; // '\n' isn't working with the js csv data extraction
+
+        // Transform the rows to CSV
+        each(rows, function (row, i) {
 
             // Add the values
             csv += row.join(itemDelimiter);
 
             // Add the line delimiter
-            if (i < rowArr.length - 1) {
+            if (i < rows.length - 1) {
                 csv += lineDelimiter;
             }
         });
-
         return csv;
     };
+
+    /**
+     * Build a HTML table with the data
+     */
+    Highcharts.Chart.prototype.getTable = function () {
+        var html = '<table>',
+            rows = this.getDataRows();
+
+        // Transform the rows to HTML
+        each(rows, function (row, i) {
+
+            var tag = i ? 'td' : 'th';
+
+            html += '<tr><' + tag + '>';
+
+            // Add the cells
+            html += row.join('</' + tag + '><' + tag + '>');
+
+            html += '</' + tag + '></tr>';
+
+        });
+        html += '</table>';
+        return html;
+    };
+
+    function getContent(href, extention, content, name, MIME) {
+        var a,
+            blobObject;
+        // Download attribute supported
+        if (downloadAttrSupported) {
+            a = document.createElement('a');
+            a.href = href;
+            a.target      = '_blank';
+            a.download    = name + extention;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+
+        } else if (window.Blob && window.navigator.msSaveOrOpenBlob) {
+            //Falls to msSaveOrOpenBlob if download attribute is not supported
+            blobObject = new Blob([content]);
+            window.navigator.msSaveOrOpenBlob(blobObject, name + '.' + extention);
+        } else {
+            // Fall back to server side handling
+            Highcharts.post('http://www.highcharts.com/studies/csv-export/download.php', {
+                data: content,
+                type: MIME + '.' + extention,
+                extension: extention
+            });
+        }
+    }
 
     // Add "Download CSV" to the exporting menu. Use download attribute if supported, else
     // run a simple PHP script that returns a file. The source code for the PHP script can be viewed at
@@ -88,26 +155,31 @@
         Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
             text: Highcharts.getOptions().lang.downloadCSV || 'Download CSV',
             onclick: function () {
-                var a;
+                var csv = this.getCSV();
+                getContent(
+                    'data:attachment/csv,' + csv.replace(/\n/g, '%0A'),
+                    'csv',
+                    csv,
+                    (this.title ? this.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart'),
+                    'attachment/csv'
+                );
+            }
 
-                // Download attribute supported
-                if (downloadAttrSupported) {
-                    // Client side extraction
-                    a = document.createElement('a');
-                    a.href        = 'data:attachment/csv,' + this.getCSV().replace(/\n/g, '%0A');
-                    a.target      = '_blank';
-                    a.download    = (this.title ? this.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart') + '.csv';
-                    document.body.appendChild(a);
-                    a.click();
-                    a.remove();
-
-                // Fall back to server side handling
-                } else {
-                    Highcharts.post('http://www.highcharts.com/studies/csv-export/csv.php', {
-                        csv: this.getCSV()
-                    });
-                }
-
+        }, {
+            text: Highcharts.getOptions().lang.downloadXLS || 'Download XLS',
+            onclick: function () {
+                var uri = 'data:application/vnd.ms-excel;base64,',
+                    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>'
+                        + this.getTable()
+                        + '</body></html>',
+                    base64 = function (s) { return window.btoa(decodeURIComponent(encodeURIComponent(s))); };
+                getContent(
+                    uri + base64(template),
+                    'xls',
+                    template,
+                    (this.title ? this.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart'),
+                    'application/vnd.ms-excel'
+                );
             }
         });
     }
