3088022493aae907bb2ae2f514c2453a711327d6
Torstein Hønsi <torstein@highsoft.com>
2015-05-15 12:33:12 +0200

    Published 1.3.2

--- export-csv.js
+++ export-csv.js
@@ -3,7 +3,7 @@
  *
  * Author:   Torstein Honsi
  * Licence:  MIT
- * Version:  1.3.1
+ * Version:  1.3.2
  */
 /*global Highcharts, window, document, Blob */
 (function (Highcharts) {
@@ -153,7 +153,10 @@
     function getContent(chart, href, extention, content, MIME) {
         var a,
             blobObject,
-            name = (chart.title ? chart.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart');
+            name = (chart.title ? chart.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart'),
+            options = (chart.options.exporting || {}).csv || {},
+            url = options.url || 'http://www.highcharts.com/studies/csv-export/download.php';
+
         // Download attribute supported
         if (downloadAttrSupported) {
             a = document.createElement('a');
@@ -165,12 +168,13 @@
             a.remove();
 
         } else if (window.Blob && window.navigator.msSaveOrOpenBlob) {
-            //Falls to msSaveOrOpenBlob if download attribute is not supported
+            // Falls to msSaveOrOpenBlob if download attribute is not supported
             blobObject = new Blob([content]);
             window.navigator.msSaveOrOpenBlob(blobObject, name + '.' + extention);
+
         } else {
             // Fall back to server side handling
-            Highcharts.post('http://www.highcharts.com/studies/csv-export/download.php', {
+            Highcharts.post(url, {
                 data: content,
                 type: MIME,
                 extension: extention
@@ -178,45 +182,56 @@
         }
     }
 
+    /**
+     * Call this on click of 'Download CSV' button
+     */
+    Highcharts.Chart.prototype.downloadCSV = function () {
+        var csv = this.getCSV(true);
+        getContent(
+            this,
+            'data:text/csv,' + csv.replace(/\n/g, '%0A'),
+            'csv',
+            csv,
+            'text/csv'
+        );
+    };
+
+    /**
+     * Call this on click of 'Download XLS' button
+     */
+    Highcharts.Chart.prototype.downloadXLS = function () {
+        var uri = 'data:application/vnd.ms-excel;base64,',
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">' +
+                '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>' +
+                '<x:Name>Ark1</x:Name>' +
+                '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
+                '<style>td{border:none;font-family: Calibri, sans-serif;} .number{mso-number-format:"0.00";}</style>' +
+                '<meta name=ProgId content=Excel.Sheet>' +
+                '</head><body>' +
+                this.getTable(true) +
+                '</body></html>',
+            base64 = function (s) { return window.btoa(decodeURIComponent(encodeURIComponent(s))); };
+        getContent(
+            this,
+            uri + base64(template),
+            'xls',
+            template,
+            'application/vnd.ms-excel'
+        );
+    };
+
+
     // Add "Download CSV" to the exporting menu. Use download attribute if supported, else
     // run a simple PHP script that returns a file. The source code for the PHP script can be viewed at
     // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
     if (Highcharts.getOptions().exporting) {
         Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
             text: Highcharts.getOptions().lang.downloadCSV || 'Download CSV',
-            onclick: function () {
-                var csv = this.getCSV(true);
-                getContent(
-                    this,
-                    'data:text/csv,' + csv.replace(/\n/g, '%0A'),
-                    'csv',
-                    csv,
-                    'text/csv'
-                );
-            }
-
+            onclick: function () { this.downloadCSV(); }
         }, {
             text: Highcharts.getOptions().lang.downloadXLS || 'Download XLS',
-            onclick: function () {
-                var uri = 'data:application/vnd.ms-excel;base64,',
-                    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">' +
-                        '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>' +
-                        '<x:Name>Ark1</x:Name>' +
-                        '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
-                        '<style>td{border:none;font-family: Calibri, sans-serif;} .number{mso-number-format:"0.00";}</style>' +
-                        '<meta name=ProgId content=Excel.Sheet>' +
-                        '</head><body>' +
-                        this.getTable(true) +
-                        '</body></html>',
-                    base64 = function (s) { return window.btoa(decodeURIComponent(encodeURIComponent(s))); };
-                getContent(
-                    this,
-                    uri + base64(template),
-                    'xls',
-                    template,
-                    'application/vnd.ms-excel'
-                );
-            }
+            onclick: function () { this.downloadXLS(); }
         });
     }
+
 }(Highcharts));
